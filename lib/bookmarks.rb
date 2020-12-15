0023f02a30df7fad2d1298366f3d74fcad50eebb
Benedict Lawrence <BTLawrence44@gmail.com>
2020-12-15 14:20:04 +0000

    third commit

--- lib/bookmarks.rb
+++ lib/bookmarks.rb
@@ -1,14 +1,6 @@
 class Bookmarks 
 
-    attr_reader :list
-
-    def initialize
-        @list = ["list", "list"]  
+    def self.all 
+       ["list", "list"]  
     end  
-
-    def show_list 
-        @list.each do |item|  
-            item 
-        end 
-    end
 end 
