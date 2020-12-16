7abcc3f81792819f01f4228dc0f47f03aecb1e72
ZeenLamDev <Zeen.Lam93@gmail.com>
2020-12-16 15:19:34 +0000

    added db test env and also tests

--- README.md
+++ README.md
@@ -6,3 +6,9 @@ As a user
 I would like to see a list of bookmarks i regularly visit
 ```
 ![domain model](/Screenshot%202020-12-14%20at%2014.17.11.png)
+
+```
+As a time-pressed user
+So that I can save a website
+I would like to add the site's address and title to bookmark manager
+```
--- lib/bookmarks.rb
+++ lib/bookmarks.rb
@@ -3,8 +3,9 @@ require 'pg'
 class Bookmarks 
 
     def self.all
-        connection = PG.connect(dbname: 'bookmark_manager')
+         if ENV['RACK_ENV'] == 'test' ? connection = PG.connect(dbname: 'bookmark_manager_test') : connection = PG.connect(dbname: 'bookmark_manager')
+         end
         result = connection.exec("SELECT * FROM bookmarks;")
         result.map { |bookmark| bookmark['url'] }
-    end  
+    end
 end 
