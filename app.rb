18a202fa6330b658e0dc6b7c3c1033caa93cd34f
ZeenLamDev <Zeen.Lam93@gmail.com>
2020-12-16 14:05:01 +0000

    setting up a database

--- app.rb
+++ app.rb
@@ -9,11 +9,7 @@ class App < Sinatra::Base
   end 
 
   get '/bookmarks' do
-<<<<<<< HEAD
-    @bookmarks = Bookmarks.new
-=======
     @bookmarks = Bookmarks.all  
->>>>>>> 0023f02a30df7fad2d1298366f3d74fcad50eebb
 
     erb :bookmarks
   end 
--- lib/bookmarks.rb
+++ lib/bookmarks.rb
@@ -1,14 +1,6 @@
 class Bookmarks 
 
-<<<<<<< HEAD
-    attr_reader :list
-
-    def initialize
-        @list = ["Pika", "Chu"]  
-    end
-=======
-    def self.all 
+    def self.all
        ["list", "list"]  
     end  
->>>>>>> 0023f02a30df7fad2d1298366f3d74fcad50eebb
 end 
